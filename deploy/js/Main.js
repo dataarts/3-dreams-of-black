a85740b9dbc5d0da8bdff8c8c174ca5c0408e8d0
Mr.doob <info@mrdoob.com>
2011-03-03 21:45:43 +0000

    Merging with master.

--- deploy/js/Main.js
+++ deploy/js/Main.js
@@ -77,6 +77,7 @@ function start( pattern ) {
 	document.body.appendChild( gui.domElement );
 
 	gui.add( audio, 'currentTime', 0, 210, 10 ).name( 'Time' ).listen();
+	gui.add( audio, 'volume', 0, 1).name( 'Volume' );
 
 	audio.play();
 	audio.currentTime = tune.getPatternMS( pattern ) / 1000;
