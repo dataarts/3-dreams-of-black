ebba37a95790d354b0d0b30d71f070095c2c8f29
Mr.doob <info@mrdoob.com>
2011-03-04 09:00:03 +0000

    Prototype: Improved camera movements.

--- deploy/js/parts/Part1.js
+++ deploy/js/parts/Part1.js
@@ -8,7 +8,7 @@ var Part1 = function ( renderer, events ) {
 
 		camera = new THREE.QuakeCamera( {
 			fov: 50, aspect: window.innerWidth / window.innerHeight, near: 1, far: 100000,
-			movementSpeed: 1.2, lookSpeed: 0.0035, noFly: true, lookVertical: true,
+			movementSpeed: 3, lookSpeed: 0.0015, noFly: true, lookVertical: true,
 			autoForward: true
 		} );
 
@@ -19,8 +19,10 @@ var Part1 = function ( renderer, events ) {
 	this.show = function () {
 
 		camera.position.x = - 150;
-		camera.position.y = - 580;
+		camera.position.y = - 575;
 		camera.position.z = 200;
+
+		camera.lon = - 90;
 
 		renderer.setClearColor( world.scene.fog.color );
 
--- deploy/js/parts/Part2.js
+++ deploy/js/parts/Part2.js
@@ -8,8 +8,8 @@ var Part2 = function ( renderer, events ) {
 
 		camera = new THREE.QuakeCamera( {
 			fov: 60, aspect: window.innerWidth / window.innerHeight, near: 1, far: 100000,
-			movementSpeed: 3, lookSpeed: 0.0035, noFly: false, lookVertical: true, 
-			autoForward: true, heightSpeed: true, heightMin: -1500, heightMax: 1000, heightCoef: 0.0125
+			movementSpeed: 10, lookSpeed: 0.0025, noFly: false, lookVertical: true, 
+			autoForward: true
 		} );
 
 		world = new Part2World();
