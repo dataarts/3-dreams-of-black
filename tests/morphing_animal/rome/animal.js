016c9f3a2d7a87e49932d53d0c5c78c54ea68dcc
Mikael Emtinger <mikaelemtinger@Mikael-Emtingers-MacBook-Pro.local>
2011-03-17 11:48:35 +0100

    Updated morphing animal

--- tests/morphing_animal/rome/animal.js
+++ tests/morphing_animal/rome/animal.js
@@ -6,61 +6,248 @@
 ROME = {};
 
 
+// animation data
+
+ROME.AnimalAnimationData = {
+
+	// init offset, length and frame times
+
+	init: function( geometry ) {
+		
+		var animal, animalName, animalNames = [ "bison", "wolf" ];		// these are the names of all existing animals
+		var morphTargets = geometry.morphTargets;
+		var a, al, m, ml, currentTime;
+		
+		for( a = 0, al = animalNames.length; a < al; a++ ) {
+			
+			animalName  = animalNames[ a ];
+			animal      = this[ animalName ];
+			currentTime = 0;
+			
+			if( animal === undefined ) {
+				
+				animal = this[ animalName ] = [];
+				
+				for( m = 0, ml = morphTargets.length; m < ml; m++ ) {
+	
+					if( morphTargets[ m ].name.indexOf( animalName ) !== -1 ) {
+
+						animal.push( { index: m, time: currentTime } );
+						currentTime += parseInt( 1000 / 24, 10 );		// 24 fps			
+						
+					}
+					
+				}
+
+			}
+			 
+		}
+
+	}
+	
+}
+
 
 // animal
 
 ROME.Animal = function( geometry ) {
 
+	// init data
+	
+	ROME.AnimalAnimationData.init( geometry );
+	
+
 	// construct
 
-	var that = {};
 	var material = new THREE.MeshShaderMaterial( { uniforms:       ROME.AnimalShader.uniforms, 
 												   vertexShader:   ROME.AnimalShader.vertex, 
 												   fragmentShader: ROME.AnimalShader.fragment,
-												   morphTargets:   true } ); 
+												   morphTargets:   true,
+												   lights:         true } ); 
 
+	var that = {};
 	that.mesh = new THREE.Mesh( geometry, material );
 	that.morph = 0.0;
-
+	that.animalA = { frames: undefined, currentFrame: 0, lengthInFrames: 0, currentTime: 0, lengthInMS: 0, timeScale: 1.0 };
+	that.animalB = { frames: undefined, currentFrame: 0, lengthInFrames: 0, currentTime: 0, lengthInMS: 0, timeScale: 1.0 };
 	
-	var animalAIndex  = 0;
-	var animalAOffset = 0;
-	var animalALength = 29;
 	
-	var animalBIndex  = 0;
-	var animalBOffset = 30;
-	var animalBLength = 100;
-	
+	var isPlaying = false;
 	var morphTargetOrder = that.mesh.morphTargetForcedOrder;
 
 
-	THREE.AnimationHandler.addToUpdate( that );
+	//--- play ---
 
-
-	// play
-
-	that.play = function( offset ) {
+	that.play = function( animalA, animalB, morph, startTimeAnimalA, startTimeAnimalB ) {
 		
+		if( !isPlaying ) {
+
+			setAnimalData( animalA, that.animalA );
+			setAnimalData( animalB, that.animalB );
+			
+			that.animalA.currentTime = startTimeAnimalA ? startTimeAnimalA : 0;
+			that.animalB.currentTime = startTimeAnimalB ? startTimeAnimalB : 0;
+			
+			isPlaying = true;
+			that.morph = 0;
+
+			THREE.AnimationHandler.addToUpdate( that );
+
+		}
 		
+		that.update( 0 );
+
 	} 
 
-	// update
+
+	//--- update ---
 	
 	that.update = function( deltaTimeMS ) {
 		
-		morphTargetOrder[ 0 ] = animalAOffset + (( animalAIndex + 0 ) % animalALength );
-		morphTargetOrder[ 1 ] = animalAOffset + (( animalAIndex + 1 ) % animalALength );
-		morphTargetOrder[ 2 ] = animalBOffset + (( animalBIndex + 0 ) % animalBLength );
-		morphTargetOrder[ 3 ] = animalBOffset + (( animalBIndex + 1 ) % animalBLength );
+		var data, dataNames = [ "animalA", "animalB" ];
+		var d, dl;
+		var f, fl;
+		var frame, nextFrame;
+		var time, nextTime;
+		var unloopedTime;
+		var lengthInMS;
+		var lenghtInFrames;
+		var morphTarget;
+		var scale;
 		
-		animalAIndex++;
-		animalBIndex++;
-		
+		for( d = 0, dl = dataNames.length, morphTarget = 0; d < dl; d++ ) {
+			
+			data = that[ dataNames[ d ]];
+			
+			unloopedTime = data.currentTime;
+			data.currentTime = ( data.currentTime + deltaTimeMS * data.timeScale ) % data.lengthInMS;
+
+
+			// did we loop?
+
+			if( unloopedTime > data.currentTime ) {
+
+				data.currentFrame = 0;				
+
+			}
+
+
+			// find frame/nextFrame
+			
+
+			frame = 0;
+			
+			for( f = data.currentFrame, fl = data.lengthInFrames - 1; f < fl; f++ ) {
+				
+				if( data.currentTime >= data.frames[ f ].time && data.currentTime < data.frames[ f + 1 ].time ) {
+					
+					frame = f;
+					break;
+				}
+			}
+
+			data.currentFrame = frame;
+			nextFrame = frame + 1 < fl ? frame + 1 : 0;
+
+			
+			morphTargetOrder[ morphTarget++ ] = data.frames[ frame     ].index;
+			morphTargetOrder[ morphTarget++ ] = data.frames[ nextFrame ].index;
+			
+			time     = data.frames[ frame     ].time;
+			nextTime = data.frames[ nextFrame ].time > time ? data.frames[ nextFrame ].time : data.frames[ nextFrame ].time + data.lengthInMS; 
+			
+			scale = ( data.currentTime - time ) / ( nextTime - time ) ;
+			
+			material.uniforms[ dataNames[ d ] + "Interpolation" ].value = scale;
+
+		}
+
 		material.uniforms.animalMorphValue.value = that.morph;
 	}
 
+
+	//--- set new target animal ---
 	
-	// public
+	that.setNewTargetAnimal = function( animal, startTimeAnimalB ) {
+		
+		if( that.morph === 1 ) {
+			
+			// switch so B -> A
+			
+			for( var property in that.animalA ) {
+				
+				that.animalA[ property ] = that.animalB[ property ];
+				
+			}
+			
+			
+			// set new B and change morph
+			
+			that.animalB.currentTime = startTimeAnimalB ? startTimeAnimalB : 0;
+			setAnimalData( animal, that.animalB );
+			setFrame( that.animalB );
+			that.morph = 0;
+			
+		}
+		else {
+			
+			console.log( "Error: Cannot change animal target if morph != 1. Skipping." );
+		}
+		
+	}
+
+
+	//--- set animal data ---
+
+	var setAnimalData = function( name, data ) {
+		
+		if( ROME.AnimalAnimationData[ name ] !== undefined ) {
+			
+			data.frames         = ROME.AnimalAnimationData[ name ];
+			data.lengthInFrames = data.frames.length;
+			data.lengthInMS     = data.frames[ data.lengthInFrames - 1 ].time;
+			
+		} else {
+			
+			console.log( "Error: Couldn't find data for animal " + name );
+			
+		}
+		
+	}
+	
+	
+	//--- set frame ---
+	
+	var setFrame = function( data ) {
+		
+		var f, fl;
+		var currentTime = data.currentTime;
+		var frames = data.frames;
+		
+		for( f = 0, fl < frames.length; f < fl; f++ ) {
+			
+			if( currentTime >= frames[ f ].time ) {
+				
+				data.currentFrame = f;
+				return;
+				
+			}
+			
+		}
+		
+	}
+
+
+	//--- set current frame ---
+	
+	var setCurrentFrame = function( data ) {
+		
+				
+
+	}
+
+	
+	//--- return public ---
 	
 	return that;
 }
@@ -86,24 +273,24 @@ ROME.AnimalShader = {
 		"uniform float	animalMorphValue;",
 		"varying vec3 	vLightWeighting;",
 
-		THREE.Snippets[ "map_pars_vertex" ],
-		THREE.Snippets[ "lightmap_pars_vertex" ],
-		THREE.Snippets[ "envmap_pars_vertex" ],
-		THREE.Snippets[ "lights_pars_vertex" ],
-		THREE.Snippets[ "color_pars_vertex" ],
+		THREE.ShaderChunk[ "map_pars_vertex" ],
+		THREE.ShaderChunk[ "lightmap_pars_vertex" ],
+		THREE.ShaderChunk[ "envmap_pars_vertex" ],
+		THREE.ShaderChunk[ "lights_pars_vertex" ],
+		THREE.ShaderChunk[ "color_pars_vertex" ],
 
 		"void main() {",
 
 			"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
 
-			THREE.Snippets[ "map_vertex" ],
-			THREE.Snippets[ "lightmap_vertex" ],
-			THREE.Snippets[ "envmap_vertex" ],
-			THREE.Snippets[ "color_vertex" ],
+			THREE.ShaderChunk[ "map_vertex" ],
+			THREE.ShaderChunk[ "lightmap_vertex" ],
+			THREE.ShaderChunk[ "envmap_vertex" ],
+			THREE.ShaderChunk[ "color_vertex" ],
 
 			"vec3 transformedNormal = normalize( normalMatrix * normal );",
 
-			THREE.Snippets[ "lights_vertex" ],
+			THREE.ShaderChunk[ "lights_vertex" ],
 
 			
 			"vec3 animalA = mix( morphTarget0, morphTarget1, animalAInterpolation );",
@@ -122,22 +309,22 @@ ROME.AnimalShader = {
 
 		"varying vec3 vLightWeighting;",
 
-		THREE.Snippets[ "color_pars_fragment" ],
-		THREE.Snippets[ "map_pars_fragment" ],
-		THREE.Snippets[ "lightmap_pars_fragment" ],
-		THREE.Snippets[ "envmap_pars_fragment" ],
-		THREE.Snippets[ "fog_pars_fragment" ],
+		THREE.ShaderChunk[ "color_pars_fragment" ],
+		THREE.ShaderChunk[ "map_pars_fragment" ],
+		THREE.ShaderChunk[ "lightmap_pars_fragment" ],
+		THREE.ShaderChunk[ "envmap_pars_fragment" ],
+		THREE.ShaderChunk[ "fog_pars_fragment" ],
 
 		"void main() {",
 
 			"gl_FragColor = vec4( diffuse, opacity );",
 			"gl_FragColor = gl_FragColor * vec4( vLightWeighting, 1.0 );",
 
-			THREE.Snippets[ "map_fragment" ],
-			THREE.Snippets[ "lightmap_fragment" ],
-			THREE.Snippets[ "color_fragment" ],
-			THREE.Snippets[ "envmap_fragment" ],
-			THREE.Snippets[ "fog_fragment" ],
+			THREE.ShaderChunk[ "map_fragment" ],
+			THREE.ShaderChunk[ "lightmap_fragment" ],
+			THREE.ShaderChunk[ "color_fragment" ],
+			THREE.ShaderChunk[ "envmap_fragment" ],
+			THREE.ShaderChunk[ "fog_fragment" ],
 
 		"}"
 
@@ -145,18 +332,3 @@ ROME.AnimalShader = {
 }
 
 
-
-	fragment: [
-
-		"uniform vec3 diffuse;",
-		"uniform float opacity;",
-
-		"varying vec3 vLightWeighting;",
-
-		"void main() {",
-
-			"gl_FragColor = vec4( diffuse, opacity );",
-			"gl_FragColor = gl_FragColor * vec4( vLightWeighting, 1.0 );",
-		"}"
-
-	].join("\n")
