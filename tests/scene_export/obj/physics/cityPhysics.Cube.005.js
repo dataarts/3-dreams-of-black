6c3f9da07364bbdf4f3ead7fd18d5af061c048df
alteredq <postfilter@gmail.com>
2011-03-25 13:39:09 +0100

    Added test scene exported from Blender.
    First version that starts to look like what's in Blender. Still not all rotations are proper and colliders are just normal meshes.

---
+++ tests/scene_export/obj/physics/cityPhysics.Cube.005.js
@@ -0,0 +1,42 @@
+/*
+ * File generated with Blender 2.56 Exporter
+ * https://github.com/mrdoob/three.js/tree/master/utils/exporters/blender/
+ *
+ * vertices: 8
+ * faces: 6
+ * normals: 0
+ * uvs: 0
+ * colors: 0
+ * materials: 1
+ *
+ */
+
+var model = {
+
+    'version' : 2,
+    
+    'materials': [	{
+	"DbgColor" : 16711680,
+	"DbgIndex" : 0,
+	"DbgName" : "default",
+	"vertexColors" : false,
+	"wireframe" : true
+	}],
+
+    'vertices': [1.000000,-1.000000,-1.000000,1.000000,-1.000000,1.000000,-1.000000,-1.000000,1.000000,-1.000000,-1.000000,-1.000000,1.000000,6.154813,-1.000000,0.999999,6.154813,1.000000,-1.000000,6.154813,0.999999,-1.000000,6.154813,-1.000000],
+
+    'morphTargets': [],
+
+    'normals': [],
+
+    'colors': [],
+
+    'uvs': [[]],
+
+    'faces': [3,0,1,2,3,0,3,4,7,6,5,0,3,0,4,5,1,0,3,1,5,6,2,0,3,2,6,7,3,0,3,4,0,3,7,0],
+
+    'end': (new Date).getTime()
+
+}
+
+postMessage( model );
---
+++ tests/scene_export/obj/physics/cityPhysics.Plane.js
@@ -0,0 +1,42 @@
+/*
+ * File generated with Blender 2.56 Exporter
+ * https://github.com/mrdoob/three.js/tree/master/utils/exporters/blender/
+ *
+ * vertices: 4
+ * faces: 1
+ * normals: 0
+ * uvs: 0
+ * colors: 0
+ * materials: 1
+ *
+ */
+
+var model = {
+
+    'version' : 2,
+    
+    'materials': [	{
+	"DbgColor" : 16711680,
+	"DbgIndex" : 0,
+	"DbgName" : "default",
+	"vertexColors" : false,
+	"wireframe" : true
+	}],
+
+    'vertices': [1.000000,-0.000000,-1.000000,1.000000,0.000000,1.000000,-1.000000,0.000000,1.000000,-1.000000,-0.000000,-1.000000],
+
+    'morphTargets': [],
+
+    'normals': [],
+
+    'colors': [],
+
+    'uvs': [[]],
+
+    'faces': [3,0,3,2,1,0],
+
+    'end': (new Date).getTime()
+
+}
+
+postMessage( model );
