a05429843199b743aefba6d002cfc7b9954d156d
Mikael Emtinger <mikaelemtinger@Mikael-Emtingers-MacBook-Pro.local>
2011-04-03 18:21:39 +0200

    Added basic stuff for backend communication

---
+++ tests/backend/Backend.js
@@ -0,0 +1,64 @@
+/**
+ * @author Mikael Emtinger
+ */
+
+
+ROME.Backend = ( function() {
+	
+	// private vars
+	
+	that = {};
+	
+	
+	// private functions
+	
+	var myPrivateFunction = function() {
+		
+		// do something
+		
+	}
+	
+	
+	// public functions
+	
+	// save single UGO
+	// parameters = { data:JSONObject, success:callback, error:callback }
+	
+	that.saveUGO = function( parameters ) {
+		
+		myPrivateFunction();
+	}
+	
+	
+	// add point
+	// parameters = { id:UGOid, type:"", amount:1, success:callback, error:callback }
+	
+	that.addPoint = function( parameters ) {
+		
+		
+	}
+	
+	
+	// load single UGO
+	// parameters = { id:UGOid, success:callback, error:callback }
+	
+	that.loadUGO = function( parameters ) {
+		
+		
+	}
+	
+	
+	// load toplist UGOs
+	// parameters = { page:0, success:callback, error:callback }
+	
+	that.loadTopListUGO = function( parameters ) {
+		
+		
+	}
+	
+	
+	// return public
+	
+	return that;
+	
+}());
