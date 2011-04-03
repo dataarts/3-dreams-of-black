25b018d418c3d8e4576ae27bce80da67875ff234
Andreas Emtinger <Empa@Empa-iMac27.local>
2011-04-04 01:46:27 +0200

    Updated

--- tests/backend/Backend.js
+++ tests/backend/Backend.js
@@ -1,88 +1,134 @@
-/**
- * @author Mikael Emtinger
- */
-
-if( ROME === undefined ) ROME = {};
+ROME = {};
 
 ROME.Backend = ( function() {
 	
-	// private vars
+	// private vars	
 	
 	that = {};
-
-	var myPrivateVar = 10;
-	var baseURL;
-	
+	var topListIndex = 1;
+	var baseURL = "services/";
 	
 	// private functions
 	
-	var myPrivateFunction = function() {
-		
-		// do something
-		
+	// return value or default value
+	
+	var getValueOrDefault = function( value, defaultValue ) {
+		return ( value === undefined ) ? defaultValue : value;
 	}
-
-	var onSaveUGO = function( response ) {
+	
+	// return callback if it exist, else false
+	
+	var getCallback = function( callback ) {
+		return getValueOrDefault( callback, false );
+	}	
+	
+	
+	// public functions
+	
+	// add single UGO
+	// parameters = { data:JSONObject, success:callback, error:callback }
+	
+	that.addUGO = function( parameters ) {
 		
-		if( response.success === "true" ) {
-			
-			lastCallbackSuccess( repsonse );
-		
-		} else {
-			
-			lastCallbackError( response );
-			
+		if(parameters.data !== undefined)
+		{		
+			var url = baseURL + "add/?object=" + parameters.data;			
+	
+			jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'text','post');
 		}
 		
 	}
 	
 	
-	// public functions
+	// add points
+	// parameters = { id:UGOid, amount:1, success:callback, error:callback }
 	
-	// save single UGO
-	// parameters = { data:JSONObject, success:callback, error:callback }
+	that.addPoints = function( parameters ) {
+		
+		if(parameters.id !== undefined)
+		{		
+			var amount = getValueOrDefault( parameters.amount, 1 )				
+			
+			var url = baseURL + "add/points/" + parameters.id + "/" + amount;			
 	
-	that.saveUGO = function( parameters ) {
+			jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'text','post');
+		}
 		
-		var url = baseURL + ObjectToJSONString( parameters.data ); 
+	}
 		
-		lastCallbackSuccess = parameters.success;
-		lastCallbackError = parameters.error;
-		isRequestion = true;
-
-		request.send( POST, url, onSaveUGO );
+	// add stars - will be removed after testing (only for administrators)
+	// parameters = { id:UGOid, amount:1, success:callback, error:callback }
+	
+	that.addStars = function( parameters ) {
+		
+		if(parameters.id !== undefined)
+		{		
+			var amount = getValueOrDefault( parameters.amount, 1 );				
+			
+			var url = baseURL + "add/stars/" + parameters.id + "/" + amount;			
+	
+			jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'text','post');
+		}
+		
+	}	
+	
+	// disabled or enable UGO - will be removed after testing (only for administrators)
+	// parameters = { id:UGOid, bool:true/false, success:callback, error:callback }
+	
+	that.disableUGO = function( parameters ) {
+		
+		if(parameters.id !== undefined)
+		{		
+			var bool = getValueOrDefault( parameters.bool, "false" );				
+			
+			var url = baseURL + "update/disable/" + parameters.id + "/" + bool;			
+	
+			jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'json','post');
+		}
+		
+	}	
+	
+	// get single UGO
+	// parameters = { id:UGOid, success:callback, error:callback }	
+	
+	that.getUGO = function( parameters ) {
+		
+		if(parameters.id !== undefined)
+		{				
+			var url = baseURL + "get/" + parameters.id;			
+	
+			jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'json','get');
+		}		
+	}	
+	
+	// get toplist for UGOs
+	// parameters = { index:0, success:callback, error:callback }	
+	
+	that.getTopListForIndex = function( parameters ) {
+		
+		var index = (parameters.index === undefined) 
+			? topListIndex 
+			: parameters.index;				
+		
+		var url = baseURL + "toplist/" + index;		
+		topListIndex = index + 1;
+		
+		jx.load(url, getCallback(parameters.success), getCallback(parameters.error), 'json','get');
+		
+	}
+	
+	// get next toplist for UGOs	
+	// parameters = { success:callback, error:callback }
+	
+	that.getNextTopList = function( parameters ) {
+		
+		parameters.index = topListIndex;
+		that.getTopListForIndex( parameters );
 		
 	}
 	
 	
-	// add point
-	// parameters = { id:UGOid, type:"", amount:1, success:callback, error:callback }
-	
-	that.addPoint = function( parameters ) {
-		
-		
-	}
-	
-	
-	// load single UGO
-	// parameters = { id:UGOid, success:callback, error:callback }
-	
-	that.loadUGO = function( parameters ) {
-		
-		
-	}
-	
-	
-	// load toplist UGOs
-	// parameters = { page:0, success:callback, error:callback }
-	
-	that.loadTopListUGO = function( parameters ) {
-		
-		
-	}
-	
-	
-	// return public
+	// return public	
 	
 	return that;
 	
