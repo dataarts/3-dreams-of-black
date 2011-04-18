1e0f29be065509f67963ded8d4a790d952c37987
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-04-18 22:51:25 +0200

    Updated effects.

--- deploy/js/effects/ClearEffect.js
+++ deploy/js/effects/ClearEffect.js
@@ -19,7 +19,14 @@ var ClearEffect = function ( shared ) {
 	this.update = function ( i ) {
 
 		renderer.clear();
-		renderer.render( scene, camera, renderTarget, true );
+		
+		var gl = renderer.getContext();
+
+		gl.bindFramebuffer( gl.FRAMEBUFFER, renderTarget.__webglFramebuffer );
+		gl.viewport( 0, 0, renderTarget.width, renderTarget.height );
+		gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT );
+
+		//renderer.render( scene, camera, renderTarget, true );
 
 	};
 
