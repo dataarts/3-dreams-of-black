1d0fea84d10260d48eb455015f3ffd2b7763a025
alteredq <postfilter@gmail.com>
2011-04-26 13:52:26 +0200

    Prototype: experimented with PaintDarkEffect, using Mikael's modification to Eskil's shader.
    Less line artefacts but it gets much darker.

--- deploy/js/effects/PaintDarkEffect.js
+++ deploy/js/effects/PaintDarkEffect.js
@@ -1,4 +1,4 @@
-var PaintEffect = function ( shared ) {
+var PaintDarkEffect = function ( shared ) {
 
 	SequencerItem.call( this );
 
@@ -47,30 +47,30 @@ var PaintEffect = function ( shared ) {
 					"vec2 uv = vUv + vec2( sin( vUv.y * 100.0 ), sin( vUv.x * 100.0 )) * 0.0005;",
 					
 					"color = texture2D( map, uv );",
-					
+
 					"add = tmp = texture2D( map, uv + vec2( 0.0015, 0.0015 ));", 
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( -0.0015, 0.0015 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( -0.0015, -0.0015 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( 0.0015, -0.0015 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( 0.002, 0.0 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( -0.002, 0.0 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( 0, 0.002 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"add += tmp = texture2D( map, uv + vec2( 0, -0.002 ));",
-					"if( tmp.r > color.r ) color = tmp;",
+					"if( tmp.r < color.r ) color = tmp;",
 
 					"uv = (uv - vec2(0.5)) * vec2(0.7);",
 					"gl_FragColor = vec4(mix(color.rgb * color.rgb * vec3(1.8), color.ggg * color.ggg - vec3(0.4), vec3(dot(uv, uv))), 1.0);",
@@ -98,5 +98,5 @@ var PaintEffect = function ( shared ) {
 
 };
 
-PaintEffect.prototype = new SequencerItem();
-PaintEffect.prototype.constructor = PaintEffect;
+PaintDarkEffect.prototype = new SequencerItem();
+PaintDarkEffect.prototype.constructor = PaintDarkEffect;
