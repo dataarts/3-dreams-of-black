71d2b39be0799ecfb7f5aca43bb973f426d0c91b
Bartek Drozdz <bartek@everyday3d.com>
2011-04-27 18:04:15 +0200

    Added paralax video

--- deploy/js/sequences/VideoSequence.js
+++ deploy/js/sequences/VideoSequence.js
@@ -1,92 +1,3 @@
-var HalfAlphaShaderSource = {
-
-	'halfAlpha' : {
-
-		uniforms: {
-
-			"map" : { type: "t", value: 0, texture: null },
-			"flip": { type: "i", value: 0 }
-
-		},
-
-		vertexShader: [
-
-			"varying vec2 vUv;",
-			"uniform bool flip;",
-
-			"void main() {",
-
-				"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-				"if ( flip )",
-					"vUv = vec2( 1.0 - uv.x, uv.y );",
-				"else",
-					"vUv = uv;",
-
-			"}"
-
-		].join("\n"),
-
-		fragmentShader: [
-
-			"uniform sampler2D map;",
-
-			"varying vec2 vUv;",
-
-			"void main() {",
-				"vec4 c = texture2D( map, vec2( vUv.x * 0.5, vUv.y ) );",
-				"vec4 a = texture2D( map, vec2( 0.5 + vUv.x * 0.5, vUv.y ) );",
-				"gl_FragColor = vec4(c.rgb, a.r);",
-			"}"
-
-		].join("\n")
-
-	},
-
-	'opaque' : {
-
-		uniforms: {
-
-			"map" : { type: "t", value: 0, texture: null },
-			"flip": { type: "i", value: 0 }
-
-		},
-
-		vertexShader: [
-
-			"varying vec2 vUv;",
-			"uniform bool flip;",
-
-			"void main() {",
-
-				"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-				"if ( flip )",
-					"vUv = vec2( 1.0 - uv.x, uv.y );",
-				"else",
-					"vUv = uv;",
-
-			"}"
-
-		].join("\n"),
-
-		fragmentShader: [
-
-			"uniform sampler2D map;",
-
-			"varying vec2 vUv;",
-
-			"void main() {",
-				"vec4 c = texture2D( map, vUv );",
-				"gl_FragColor = c;",
-			"}"
-
-		].join("\n")
-
-	}
-
-};
-
 var VideoSequence = function ( shared, videoPath, hasAlpha, flip ) {
 
 	SequencerItem.call( this );
@@ -126,7 +37,7 @@ var VideoSequence = function ( shared, videoPath, hasAlpha, flip ) {
 		texture.magFilter = THREE.LinearFilter;
 
 		if ( hasAlpha ) {
-			var shader = HalfAlphaShaderSource['halfAlpha'];
+			var shader = VideoShadersSource['halfAlpha'];
 
 			var uniforms = THREE.UniformsUtils.clone( shader.uniforms );
 			uniforms['map'].texture = texture;
@@ -144,7 +55,7 @@ var VideoSequence = function ( shared, videoPath, hasAlpha, flip ) {
 
 		} else {
 
-			var shader = HalfAlphaShaderSource['opaque'];
+			var shader = VideoShadersSource['opaque'];
 
 			var uniforms = THREE.UniformsUtils.clone( shader.uniforms) ;
 			uniforms['map'].texture = texture;
