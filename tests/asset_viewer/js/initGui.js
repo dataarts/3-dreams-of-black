136306733469e78c358e85f652b9373f89890658
arodic <aleksandar.xyz@gmail.com>
2011-04-27 19:25:41 -0700

    added ssao and depth of fiield to asset viewer

--- tests/asset_viewer/js/initGui.js
+++ tests/asset_viewer/js/initGui.js
@@ -5,17 +5,21 @@ function initGui(){
     "speed": 0.3,
     "morph": 1,
     "texture": true,
+    "grid": true,
     "wireframe": false,
     "auto": true,
     "lighting": true,
     "shader_components": 1,
     "depth_of_field": false,
     "focus": 0.33,
-    "aperture": 0.025
+    "aperture": 0.025,
+    "occlusion": false,
+    "radius": 0.05
   };
   gui = new GUI({domElement:  document.getElementById('viewerSettings')});
   gui.add(params, "speed", 0, 1).listen();
   gui.add(params, "morph", 0, 1).listen().onChange(function(){ params.auto = false; });
+  gui.add(params, 'grid', true).listen();
   gui.add(params, 'wireframe', false).listen();
   gui.add(params, 'texture', true).listen();
   gui.add(params, 'lighting', true).listen();
@@ -23,4 +27,6 @@ function initGui(){
   gui.add(params, 'depth_of_field', false).listen();
   gui.add(params, "focus", 0, 1).listen();
   gui.add(params, "aperture", 0, 0.1).listen();
+  gui.add(params, "occlusion", false).listen();
+  gui.add(params, "radius", 0, 0.1).listen();
 }
--- tests/asset_viewer/js/rendering.js
+++ tests/asset_viewer/js/rendering.js
@@ -37,13 +37,20 @@ function initPostprocessingNoise( effect ) {
     effect.camera = new THREE.Camera();
     effect.camera.projectionMatrix = THREE.Matrix4.makeOrtho( width / - 2, width / 2, height / 2, height / - 2, -10000, 10000 );
     effect.textureColor = new THREE.WebGLRenderTarget( width*2, height*2, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter } );
-    effect.textureDepth = new THREE.WebGLRenderTarget( width*2, height*2, { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter } );
-
+    effect.textureDepth = new THREE.WebGLRenderTarget( width*2, height*2, { minFilter: THREE.NearestFilter, magFilter: THREE.NearestFilter } );
+    effect.textureNoise = THREE.ImageUtils.loadTexture( 'files/textures/noise.png', { minFilter: THREE.LinearFilter, magFilter: THREE.LinearFilter });
+    effect.textureNoise.minFilter = THREE.NearestFilter;
+    effect.textureNoise.wrapS = THREE.RepeatWrapping;
+     effect.textureNoise.wrapT = THREE.RepeatWrapping;
     var heatUniforms = {
     "time": { type: "f", value: 0 },
+    "sphere": { type: "fv", value: [0.000000,0.000000,-1.000000,0.000000,0.525731,-0.850651,0.500000,0.162460,-0.850651,0.000000,0.894427,-0.447213,0.500000,0.688191,-0.525731,0.850651,0.276394,-0.447213,0.309017,-0.425325,-0.850651,0.809017,-0.262865,-0.525731,0.525731,-0.723607,-0.447213,-0.309017,-0.425325,-0.850651,0.000000,-0.850651,-0.525731,-0.525731,-0.723607,-0.447213,-0.500000,0.162460,-0.850651,-0.809017,-0.262865,-0.525731,-0.850651,0.276394,-0.447213,-0.500000,0.688191,-0.525731,-0.309017,0.951057,0.000000,-0.809017,0.587785,0.000000,-0.525731,0.723607,0.447213,-1.000000,0.000000,0.000000,-0.809017,-0.587785,0.000000,-0.850651,-0.276394,0.447213,-0.309017,-0.951057,0.000000,0.309017,-0.951057,0.000000,0.000000,-0.894427,0.447213,0.809017,-0.587785,0.000000,1.000000,0.000000,0.000000,0.850651,-0.276394,0.447213,0.809017,0.587785,0.000000,0.309017,0.951057,0.000000,0.525731,0.723607,0.447213,0.000000,0.850651,0.525731,-0.809017,0.262865,0.525731,-0.500000,-0.688191,0.525731,0.500000,-0.688191,0.525731,0.809017,0.262865,0.525731,0.000000,0.000000,1.000000,0.309017,0.425325,0.850651,-0.309017,0.425325,0.850651,0.500000,-0.162460,0.850651,0.000000,-0.525731,0.850651,-0.500000,-0.162460,0.850651] },
     "tColor": { type: "t", value: 0, texture: effect.textureColor },
     "tDepth": { type: "t", value: 1, texture: effect.textureDepth },
+    "tNoise": { type: "t", value: 2, texture: effect.textureNoise },
     "dof":    { type: "f", value: 0.0 },
+    "ssao":    { type: "f", value: 0.0 },
+    "ssaoRad":    { type: "f", value: 0.0 },
     "focus":    { type: "f", value: 0.33 },
 		"aspect":   { type: "f", value: aspect },
 		"aperture": { type: "f", value: 0.025 },
@@ -51,6 +58,7 @@ function initPostprocessingNoise( effect ) {
     };
 
     effect.materialHeat = new THREE.MeshShaderMaterial( {
+
 
         uniforms: heatUniforms,
         vertexShader: [
@@ -70,11 +78,14 @@ function initPostprocessingNoise( effect ) {
 
         "uniform sampler2D tColor;",
         "uniform sampler2D tDepth;",
+        "uniform sampler2D tNoise;",
 
         "uniform float maxblur;",  	// max blur amount
         "uniform float aperture;",	// aperture - bigger values for shallower depth of field
         "uniform float dof;",
-
+        "uniform float ssao;",
+        "uniform float ssaoRad;",
+        "uniform vec3 sphere[42];",
         "uniform float focus;",
         "uniform float aspect;",
 
@@ -83,6 +94,27 @@ function initPostprocessingNoise( effect ) {
 				"void main() {",
 
           "vec4 col = vec4( 0.0 );",
+          "float ao = 1.0;",
+
+          "if (ssao == 1.0) {",
+            "float ez = texture2D(tDepth, vUv).r;",
+
+            "vec4 pl = texture2D( tNoise, vUv*vec2(30.0,30.0)*vec2(ez,-ez));",
+
+            "vec3 df = vec3(0.0);",
+
+            "for( int i=1; i<42; i++ ){",
+
+              "vec3 se = vec3(vUv,ez) + ssaoRad*reflect(sphere[i].xyz,pl.xyz);",
+
+              "float sz = texture2D(tDepth,se.xy).r;",
+              "float zd = (se.z-sz);",
+              "zd = max(min(zd-0.06,0.22-zd), 0.0);",
+              "ao += 1.0/(1.0+7000.0*zd*zd);",
+            "}",
+
+            "ao = ao/42.0;",
+          "}",
 
           "if (dof == 1.0) {",
             "vec2 aspectcorrect = vec2( 1.0, aspect );",
@@ -142,26 +174,16 @@ function initPostprocessingNoise( effect ) {
             "col += texture2D( tColor, vUv.xy + ( vec2( -0.29, -0.29 ) * aspectcorrect ) * dofblur4 );",
             "col += texture2D( tColor, vUv.xy + ( vec2(  0.0,   0.4  ) * aspectcorrect ) * dofblur4 );",
 
-            "gl_FragColor = col / 41.0;",
+            "col = col / 41.0;",
           "} else {",
-
-          //ANTIALIASING
-					"col = texture2D( tColor, vUv );",
-          "col += texture2D( tColor, vUv+vec2(.5/960.,0.) );",
-          "col += texture2D( tColor, vUv+vec2(.0,.5/360.) );",
-          "col += texture2D( tColor, vUv+vec2(.5/960.,.5/360.) );",
-          "col *= 0.25;",
-
-					"vec4 depth;",
-          //ANTIALIASING OFF
-					"depth = texture2D( tDepth, vUv );",
-          "depth += texture2D( tDepth, vUv+vec2(.5/960.,0.) );",
-          "depth += texture2D( tDepth, vUv+vec2(.0,.5/360.) );",
-          "depth += texture2D( tDepth, vUv+vec2(.5/960.,.5/360.) );",
-          "depth *= 0.25;",
-
-          "gl_FragColor = col;",
+            //ANTIALIASING
+            "col = texture2D( tColor, vUv );",
+            "col += texture2D( tColor, vUv+vec2(.5/960.,0.) );",
+            "col += texture2D( tColor, vUv+vec2(.0,.5/360.) );",
+            "col += texture2D( tColor, vUv+vec2(.5/960.,.5/360.) );",
+            "col *= 0.25;",
           "}",
+          "gl_FragColor = col*vec4(ao,ao,ao,1.0);",
 				"}"
 
             ].join("\n")
@@ -176,22 +198,25 @@ function initPostprocessingNoise( effect ) {
 function render(){
     renderer.clear();
 
+    wireMat.opacity = params.grid;
     lightmapShader.uniforms['shaderDebug'].value = params.shader_components;
     triggerShader.uniforms['shaderDebug'].value = params.shader_components;
     animalShader.uniforms['shaderDebug'].value = params.shader_components;
-    //wireMat.opacity = 0.2;
+
 
     renderer.render( scene, camera, postprocessing.textureColor, true );
 
     lightmapShader.uniforms['shaderDebug'].value = 3;
     triggerShader.uniforms['shaderDebug'].value = 3;
     animalShader.uniforms['shaderDebug'].value = 3;   
-    //wireMat.opacity = 0.0;
+    wireMat.opacity = 0.0;
 
     renderer.render( scene, camera, postprocessing.textureDepth, true );
 
 
-    postprocessing.materialHeat.uniforms.dof.value = params.depth_of_field;
+    postprocessing.materialHeat.uniforms.ssao.value = params.occlusion;
+    postprocessing.materialHeat.uniforms.ssaoRad.value = params.radius;
+    postprocessing.materialHeat.uniforms.dof.value = params.depth_of_field
     postprocessing.materialHeat.uniforms.focus.value = params.focus;
     postprocessing.materialHeat.uniforms.aperture.value = params.aperture;
   
@@ -199,6 +224,7 @@ function render(){
     postprocessing.quad.materials[ 0 ] = postprocessing.materialHeat;
     postprocessing.materialHeat.uniforms.tColor.texture = postprocessing.textureColor;
     postprocessing.materialHeat.uniforms.tDepth.texture = postprocessing.textureDepth;
+    postprocessing.materialHeat.uniforms.tNoise.texture = postprocessing.textureNoise;
     renderer.render( postprocessing.scene, postprocessing.camera );
 
 
@@ -206,7 +232,7 @@ function render(){
 }
 
 function updateCamera(){
-    near = Math.max(0.1,(-camera.position.x) - Math.max(meshRadius+100, 400));
-    far = (-camera.position.x) + meshRadius + Math.max(meshRadius+100, 400);
+    near = Math.max(0.1,(-camera.position.x) - Math.max(meshRadius+100, 300));
+    far = (-camera.position.x) + Math.max(meshRadius+100, 300);
     camera.projectionMatrix = THREE.Matrix4.makePerspective(45, aspect, near, far);
 }
