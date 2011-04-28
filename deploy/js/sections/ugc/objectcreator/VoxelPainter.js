adc820086cb4ca8f92fcc1b1bb39b5774b294de5
Mr.doob <info@mrdoob.com>
2011-04-29 01:44:45 +0800

    UGC Tool: Added preview voxel.

--- deploy/js/sections/ugc/objectcreator/VoxelPainter.js
+++ deploy/js/sections/ugc/objectcreator/VoxelPainter.js
@@ -32,7 +32,7 @@ var VoxelPainter = function () {
 	_collider.visible = false;
 
 	var _geometry = new THREE.Plane( 2000, 2000, 16, 16 );
-	var _material = new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.2, transparency: true, wireframe: true } );
+	var _material = new THREE.MeshBasicMaterial( { color: 0x000000, opacity: 0.2, transparent: true, wireframe: true } );
 
 	_plane = new THREE.Mesh( _geometry, _material );
 	_plane.doubleSided = true;
@@ -58,15 +58,28 @@ var VoxelPainter = function () {
 	var _geometry = new THREE.Cube( _size, _size, _size );
 	var _material = new THREE.MeshLambertMaterial( { color: 0xffffff } );
 
+	// Preview
+
+	var _preview = new THREE.Mesh( _geometry, new THREE.MeshLambertMaterial( { color: 0xff0000, opacity: 0, transparent: true, depthTest: false } ) );
+	_preview.doubleSided = true;
+	_preview.matrixAutoUpdate = false;
+	_scene.addObject( _preview );
+
 	//
 
 	addVoxel( new THREE.Vector3() );
 
+	function toGridScale( value ) {
+
+		return Math.round( value / _size );
+
+	}
+
 	function addVoxel( vector ) {
 
-		var x = Math.round( vector.x / _size );
-		var y = Math.round( vector.y / _size );
-		var z = Math.round( vector.z / _size );
+		var x = toGridScale( vector.x );
+		var y = toGridScale( vector.y );
+		var z = toGridScale( vector.z );
 
 		if ( _grid[ x + "." + y + "." + z ] == null ) {
 
@@ -87,9 +100,9 @@ var VoxelPainter = function () {
 
 	function removeVoxel( voxel ) {
 
-		var x = Math.round( voxel.position.x / _size );
-		var y = Math.round( voxel.position.y / _size );
-		var z = Math.round( voxel.position.z / _size );
+		var x = toGridScale( voxel.position.x );
+		var y = toGridScale( voxel.position.y );
+		var z = toGridScale( voxel.position.z );
 
 		_grid[ x + "." + y + "." + z ] = null;
 
@@ -118,14 +131,25 @@ var VoxelPainter = function () {
 
 				if ( intersects.length > 0 ) {
 
-					_collider.position.copy( intersects[ 0 ].point );
+					_preview.materials[ 0 ].opacity = 0.5;
+
+					_collider.position.copy( intersects[ 0 ].point.clone().addSelf( intersects[ 0 ].face.normal ) );
 					_collider.updateMatrix();
 					_collider.update();
+
+					_preview.position.copy( _collider.position );
+					_preview.position.x = toGridScale( _preview.position.x ) * _size;
+					_preview.position.y = toGridScale( _preview.position.y ) * _size;
+					_preview.position.z = toGridScale( _preview.position.z ) * _size;
+					_preview.updateMatrix();
+					_preview.update();
 
 					_intersectedObject = intersects[ 0 ].object;
 					_intersectedFace = intersects[ 0 ].face;
 
 				} else {
+
+					_preview.materials[ 0 ].opacity = 0;
 
 					_intersectedObject = null;
 					_intersectedFace = null;
@@ -135,6 +159,8 @@ var VoxelPainter = function () {
 			break;
 
 			case VoxelPainter.MODE_DRAW:
+
+				_preview.materials[ 0 ].opacity = 0;
 
 				intersects = ray.intersectScene( _sceneCollider );
 
