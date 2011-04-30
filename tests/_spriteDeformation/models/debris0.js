5bd102f049feab1da4b845de034b18b4e3082c82
arodic <aleksandar.xyz@gmail.com>
2011-04-29 19:12:31 -0700

    added deformation test with webm videos and different grids

---
+++ tests/_spriteDeformation/models/debris0.js
@@ -0,0 +1,43 @@
+// Converted from: ./debris0.obj
+//  vertices: 4
+//  faces: 4
+//  normals: 4
+//  colors: 0
+//  uvs: 4
+//  materials: 0
+//  edges: 0
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_three.py
+
+
+var model = {
+
+    "version" : 2,
+    
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "default"
+	}],
+
+    "vertices": [0.000000,1.341133,-0.000000,-1.085170,-0.383661,0.000000,0.352076,-0.248952,0.609814,0.374523,-0.264824,-0.648693],
+    
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [0.223351,0.974286,0.029671,-0.827742,-0.560552,0.024986,0.534273,-0.421493,0.732732,0.549774,-0.435321,-0.712913],
+
+    "colors": [],
+
+    "uvs": [[0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000,0.000000]],
+
+    "faces": [42,0,2,3,0,0,2,3,0,2,3,42,0,3,1,0,0,3,1,0,3,1,42,0,1,2,0,0,1,2,0,1,2,42,1,3,2,0,1,3,2,1,3,2],
+
+    "edges" : []
+
+};
+
+postMessage( model );
+close();
