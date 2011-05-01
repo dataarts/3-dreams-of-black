d7a112c9292711148c7b61415aee3b3dba0105c8
alteredq <postfilter@gmail.com>
2011-05-01 18:26:34 +0200

    Prototype: added video preload.

--- deploy/js/video/VideoPlane.js
+++ deploy/js/video/VideoPlane.js
@@ -1,68 +1,82 @@
-var VideoPlane = function(layer, config){
+var VideoPlane = function( layer, config ) {
+	
     var video, texture, interval, shader, material, wireMaterial;
 	var config = config;
 	var hasDistortion = false;
 	var hasKey = false;
     
-    video = document.createElement('video');
+    video = document.createElement( 'video' );
     video.src = layer.path;
+	video.preload = true;
     
     texture = new THREE.Texture(video);
     texture.minFilter = THREE.LinearFilter;
     texture.magFilter = THREE.LinearFilter;
     
-    switch (layer.shaderId) {
+    switch ( layer.shaderId ) {
+
         case VIDEO_OPAQUE:
             shader = VideoShaderSource.opaque;
             break;
+
 		case VIDEO_OPAQUE_DISTORT:
             shader = VideoShaderSource.distortOpaque;
 			hasDistortion = true;
             break;
+
 		case VIDEO_KEYED_DISTORT:
             shader = VideoShaderSource.distortKeyed;
 			hasDistortion = true;
 			hasKey = true;
             break;
+
 		case VIDEO_HALFALPHA:
             shader = VideoShaderSource.halfAlpha;
             break;
+
         case VIDEO_KEYED:
         default:
             shader = VideoShaderSource.keyed;
 			hasKey = true;
             break;
+
     }
 	
-	var uniforms = THREE.UniformsUtils.clone(shader.uniforms); // ? ######
+	var uniforms = THREE.UniformsUtils.clone( shader.uniforms ); // ? ######
     uniforms['map'].texture = texture;
 	
-	if (hasDistortion) {
-		uniforms['mouseXY'].value = new THREE.Vector2(0, 0);
+	if ( hasDistortion ) {
+
+		uniforms['mouseXY'].value = new THREE.Vector2( 0, 0 );
 		uniforms['aspect'].value = config.aspect;
+
 	}
 	
-	if (hasKey) {
+	if ( hasKey ) {
+
 		uniforms['colorScale'].value = layer.colorScale;
 		uniforms['threshold'].value = layer.threshold;
 		uniforms['alphaFadeout'].value = layer.alphaFadeout;
+
 	}
 	
 	material = new THREE.MeshShaderMaterial({
+
         uniforms: uniforms,
         vertexShader: shader.vertexShader,
         fragmentShader: shader.fragmentShader,
         blending: THREE.BillboardBlending
+
     });
 	
 	//
-	if(!layer.width) layer.width = (hasDistortion) ? 1.104 : 1;
-	if(!layer.height) layer.height = (hasDistortion) ? 1.24 : 1;
+	if( !layer.width ) layer.width = ( hasDistortion ) ? 1.104 : 1;
+	if( !layer.height ) layer.height = ( hasDistortion ) ? 1.24 : 1;
     
-    if(hasDistortion) 
+    if( hasDistortion ) 
 		this.mesh = new THREE.Mesh( config.grid, material );
 	else 
-		this.mesh = new THREE.Mesh( new THREE.Plane(1,1,1,1), material );
+		this.mesh = new THREE.Mesh( new THREE.Plane( 1,1,1,1 ), material );
 		
 	
 	this.mesh.scale.x = layer.width;
@@ -89,27 +103,39 @@ var VideoPlane = function(layer, config){
 	    this.wireMesh.scale.y *= Math.abs(layer.z) * config.adj;
 	}
 	
-	this.start = function(t) {
+	this.start = function( t ) {
+
 		video.currentTime = video.duration * t;
 		video.play();
 		
-		interval = setInterval(function(){
-	        if (video.readyState === video.HAVE_ENOUGH_DATA) {
+		interval = setInterval( function() {
+
+	        if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
+
 	            texture.needsUpdate = true;
+
 	        }
+
 	    }, 1000 / 24);
+
 	}
 	
 	this.stop = function() {
+		
 		video.pause();
 		clearInterval( interval );
+
 	}
 	
 	this.updateUniform = function(mouseX, mouseY) {
-		if(!hasDistortion) return;
+
+		if( !hasDistortion ) return;
+
 		material.uniforms['mouseXY'].value.x = -mouseX * config.aspect;
 		material.uniforms['mouseXY'].value.y = -mouseY;
+
 	}
+
 }
 
 
--- deploy/js/video/VideoPlayer.js
+++ deploy/js/video/VideoPlayer.js
@@ -4,7 +4,8 @@ var VIDEO_OPAQUE_DISTORT = 3;
 var VIDEO_KEYED = 4;
 var VIDEO_KEYED_DISTORT = 5;
 
-var VideoPlayer = function(shared, layers, conf){
+var VideoPlayer = function( shared, layers, conf ) {
+
 	var that = this;
 	
 	SequencerItem.call( this );
@@ -29,15 +30,19 @@ var VideoPlayer = function(shared, layers, conf){
 		config.tgd = conf.targetDistance || 1500;
 		
 		onGrid = function(geometry){
+
 			config.grid = geometry;
 			that.onLoad();
+
 		}
 		
 		gridLoader = new THREE.JSONLoader();
 		gridLoader.load( { model: "files/models/VideoDistortGrid.js", callback: onGrid } );
+
 	}
 	
-	this.onLoad = function() {	
+	this.onLoad = function() {
+
 		gridLoaded = true;
 	
 	 	shared.signals.mousemoved.add(function(){
@@ -53,35 +58,49 @@ var VideoPlayer = function(shared, layers, conf){
 		config.adj = Math.tan( config.fov * Math.PI / 360 ) * 2;
 		
 		camera = new THREE.Camera( config.fov, config.aspect, 1, 100000 );
-		camera.target.position = new THREE.Vector3(0, 0, config.tgd * -1);
+		camera.target.position = new THREE.Vector3( 0, 0, config.tgd * -1 );
 		camera.updateMatrix();
 		
 		scene = new THREE.Scene();
 		scene.addLight( new THREE.AmbientLight( 0x000000 ) );
 
 		for(var i = 0; i < layers.length; i++) {
+
 			var p = new VideoPlane(layers[i], config);
 			planes.push(p);
 			scene.addObject(p.mesh);
 			if(p.wireMesh) scene.addObject(p.wireMesh);
+
 		}
+
 	}
 	
-	this.show = function(progress) {
-		for (var i = 0; i < planes.length; i++) {
-			planes[i].start(progress);
+	this.show = function( progress ) {
+
+		for ( var i = 0; i < planes.length; i++ ) {
+
+			planes[i].start( progress );
+
 		}
+
 	}
 	
 	this.hide = function(){
-		for (var i = 0; i < planes.length; i++) {
+
+		for ( var i = 0; i < planes.length; i++ ) {
+
 			planes[i].stop();
+
 		}
+
 	}
 	
-	this.update = function(progress, delta, time) {
-		if(!gridLoaded) {
+	this.update = function( progress, delta, time ) {
+
+		if( !gridLoaded ) {
+
 			return;
+
 		}
 		
 		targetPos.x = mouseX * config.prx;
@@ -93,8 +112,10 @@ var VideoPlayer = function(shared, layers, conf){
 		camera.target.position.x += (targetPos.x - camera.target.position.x) / 2;
 		camera.target.position.y += (targetPos.y - camera.target.position.y) / 2;	
 				
-		for (var i = 0; i < planes.length; i++) {
-			planes[i].updateUniform(mouseX, mouseY);
+		for ( var i = 0; i < planes.length; i++ ) {
+
+			planes[i].updateUniform( mouseX, mouseY );
+
 		}
 		
 		renderer.render( scene, camera, renderTarget );
