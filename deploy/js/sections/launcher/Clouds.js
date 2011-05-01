d012c9a06e51e71c93bff632f795e03592ac5859
arodic <aleksandar.xyz@gmail.com>
2011-05-01 12:40:49 -0700

    adjusted birds position, blur and animaion

--- deploy/js/sections/launcher/Clouds.js
+++ deploy/js/sections/launcher/Clouds.js
@@ -30,7 +30,8 @@ var Clouds = function ( shared ) {
 
   var mouse = { x: 0, y: 0 }, vector = { x: 0, y: 0, z: 0}, delta, time, oldTime = start_time = new Date().getTime(),
 	camera, postCamera, scene, postScene, birdsScene, renderer, birdsGroup, mesh, mesh2, geometry, fog, material, postMaterial, renderTargetClouds, renderTargetFlamingos;
-
+  fog = new THREE.Fog( 0x4584b4, - 100, 3000 );
+  
 	camera = new THREE.Camera( 30, window.innerWidth / window.innerHeight, 1, 3000 );
 	camera.position.z = 6000;
 
@@ -42,7 +43,7 @@ var Clouds = function ( shared ) {
 
 	var texture = THREE.ImageUtils.loadTexture( 'files/cloud256.png', null, function () {
 
-		fog = new THREE.Fog( 0x4584b4, - 100, 3000 );
+
 
 		material = new THREE.MeshShaderMaterial( {
 
@@ -136,7 +137,10 @@ var Clouds = function ( shared ) {
 
   var postUniforms = {
     "tClouds": { type: "t", value: 0, texture: renderTargetClouds },
-    "tFlamingos": { type: "t", value: 1, texture: renderTargetFlamingos }
+    "tFlamingos": { type: "t", value: 1, texture: renderTargetFlamingos },
+    "width": { type: "f", value: window.innerWidth },
+    "height": { type: "f", value: window.innerHeight },
+    "fogColor" : {type: "c", value: fog.color}
   };
   postMaterial = new THREE.MeshShaderMaterial( {
         uniforms: postUniforms,
@@ -153,13 +157,29 @@ var Clouds = function ( shared ) {
         fragmentShader: [
           "uniform sampler2D tClouds;",
           "uniform sampler2D tFlamingos;",
+          "uniform vec3 fogColor;",
+          "uniform float width;",
+          "uniform float height;",
           "varying vec2 vUv;",
 
           "void main() {",
-              "gl_FragColor = texture2D( tFlamingos, vUv.xy );",
-              "vec4 clouds = texture2D( tClouds, vUv.xy );",
-              "gl_FragColor = mix(gl_FragColor, clouds, clouds.a);",
-              "gl_FragColor.rgb *= 1./gl_FragColor.a;",//premultiplied alpha fix
+              "vec4 flamingos = vec4(0.);",
+              "if (vUv.y > 0.5) {",
+                  "flamingos += texture2D( tFlamingos, vUv );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(1./width,0.) );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(.0,1./height) );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(1./width,1./height) );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(2./width,0.) );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(.0,2./height) );",
+                  "flamingos += texture2D( tFlamingos, vUv+vec2(2./width,2./height) );",
+                  "flamingos *= 1./7.;",
+                  "flamingos = mix(flamingos, vec4(fogColor,1.), 0.3);",
+              "}",
+
+              "vec4 clouds = texture2D( tClouds, vUv );",
+              "gl_FragColor = mix(flamingos, clouds, clouds.a);",
+              "gl_FragColor.rgb *= 1./gl_FragColor.a;",
+
           "}"
 
         ].join("\n")
@@ -178,8 +198,8 @@ var Clouds = function ( shared ) {
     for ( var i = 0; i < 2; i ++ ) {
       /////Bioids
       boid = boids[ i ] = new Boid();
-      boid.position.x = 240;
-      boid.position.y = 90 + Math.random() * 10;
+      boid.position.x = 320;
+      boid.position.y = 20 + Math.random() * 10;
       boid.position.z = 200 + Math.random();
       boid.velocity.x = Math.random() * 2 - 1;
       boid.velocity.y = Math.random() * 2 - 1;
@@ -190,9 +210,6 @@ var Clouds = function ( shared ) {
       /////Birds
       morphObject[i] = new ROME.Animal( geometry, true );
       morphObject[i].timeOffset = Math.random()*100;
-      morphObject[i].mesh.materials[0].uniforms['fogFar'].value = fog.far;
-      morphObject[i].mesh.materials[0].uniforms['fogNear'].value = fog.near;
-      morphObject[i].mesh.materials[0].uniforms['fogColor'].color = fog.color;
       morphObject[i].mesh.materials[0].fog = true;
       bird = birds[i] = morphObject[i].mesh;
       bird.phase = Math.floor( Math.random() * 62.8);
@@ -330,7 +347,7 @@ var Boid = function() {
 
     if ( _avoidWalls ) {
 
-      vector.set( - _width, this.position.y, this.position.z );
+      vector.set( - _width*15, this.position.y, this.position.z );
       vector = this.avoid( vector );
       vector.multiplyScalar( 5 );
       _acceleration.addSelf( vector );
