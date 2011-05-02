96a99c31ecdf6b03426f2270ed002feb30692d06
George Michael Brower <georgebrower@google.com>
2011-05-02 15:44:05 -0700

    started hooking up gallery to the backend

--- gallery/gallery/js/gallery.js
+++ gallery/gallery/js/gallery.js
@@ -1,4 +1,18 @@
-var GALLERY = function() {
+var GALLERY = function(params) {
+
+  if (params == undefined) {
+    params = {};
+  }
+
+  params['page'] = parseInt(params['page']);
+  if (isNaN(params['page'])) params['page'] = 1;
+
+  params['category'] = parseInt(params['category']);
+  if (isNaN(params['category'])) params['category'] = 0;
+
+  console.log(params);
+
+  var OBJECTS_PER_PAGE = 15;
 
   var gallery = $('#gallery');
   var galleryInner = $('#gallery .inner');
@@ -12,45 +26,96 @@ var GALLERY = function() {
 
   var lightboxOpen = false;
 
-  window.addEventListener('resize', onWindowResize, false)
-  onWindowResize();
+  var itemHTML = [
+    '<div class="object">',
+    '<div class="img-wrapper"><img src="%img%" alt="%title%"/></div>',
+    '<div class="object-title">',
+    '%title%',
+    '</div>',
+    '<div class="vote-box">',
+    '<a href="#" class="upvote vote"></a>',
+    '<a href="#" class="downvote vote"></a>',
+    '</div>',
+    '</div>'].join("\n");
 
-  var closeLightbox = function() {
-    lightboxOpen = true;
-    lightbox.fadeOut(200);
-    shade.get(0).removeEventListener('mousedown', closeLightbox, false);
-  };
+  createPagination();
+  populateObjects();
 
-  $('#lightbox-close').click(closeLightbox);
+  function createPagination() {
 
-  $('.object .img-wrapper').click(function() {
+    $.ajax({
+      url: GALLERY.API_BASE + 'objects?offset=' + (params['page'] - 1) * OBJECTS_PER_PAGE + '&limit=' + OBJECTS_PER_PAGE + '&category=' + params['category'],
+      success:
+        function(data) {
+          $.each(data, function(key, value) {
+            var toAdd = itemHTML;
+            toAdd = toAdd.replace(/%title%/g, value.title);
+            toAdd = toAdd.replace(/%img%/g, GALLERY.API_BASE + 'objects/' + value.id + '/thumbnail');
+            galleryInner[0].innerHTML += toAdd;
+          });
+          init();
+        },
+      error: error
+    });
 
-    lightbox.fadeIn(200);
-    shade.get(0).addEventListener('mousedown', closeLightbox, false);
+  }
 
-  });
+  function populateObjects() {
 
-  $('.upvote').click(function() {
+    $.ajax({
+      url: GALLERY.API_BASE + 'objects?offset=' + (params['page'] - 1) * OBJECTS_PER_PAGE + '&limit=' + OBJECTS_PER_PAGE + '&category=' + params['category'],
+      success:
+        function(data) {
+          $.each(data, function(key, value) {
+            var toAdd = itemHTML;
+            toAdd = toAdd.replace(/%title%/g, value.title);
+            toAdd = toAdd.replace(/%img%/g, GALLERY.API_BASE + 'objects/' + value.id + '/thumbnail');
+            galleryInner[0].innerHTML += toAdd;
+          });
+          init();
+        },
+      error: error
+    });
 
-    if (GALLERY.onupvote($(this).index('.upvote'))) {
-      $(this).next().removeClass('selected');
-      $(this).addClass('selected');
-    }
+  }
 
-    return false;
+  function init() {
 
-  });
+    window.addEventListener('resize', onWindowResize, false);
+    onWindowResize();
 
-  $('.downvote').click(function() {
+    $('#lightbox-close').click(closeLightbox);
 
-    if (GALLERY.ondownvote($(this).index('.downvote'))) {
-      $(this).prev().removeClass('selected');
-      $(this).addClass('selected');
-    }
+    $('.object .img-wrapper').click(function() {
 
-    return false;
+      lightbox.fadeIn(200);
+      shade.get(0).addEventListener('mousedown', closeLightbox, false);
 
-  });
+    });
+
+    $('.upvote').click(function() {
+
+      if (GALLERY.onupvote($(this).index('.upvote'))) {
+        $(this).next().removeClass('selected');
+        $(this).addClass('selected');
+      }
+
+      return false;
+
+    });
+
+    $('.downvote').click(function() {
+
+      if (GALLERY.ondownvote($(this).index('.downvote'))) {
+        $(this).prev().removeClass('selected');
+        $(this).addClass('selected');
+      }
+
+      return false;
+
+    });
+
+  }
 
   function onWindowResize() {
 
@@ -66,5 +131,14 @@ var GALLERY = function() {
 
   }
 
+  function closeLightbox() {
+    lightboxOpen = true;
+    lightbox.fadeOut(200);
+    shade.get(0).removeEventListener('mousedown', closeLightbox, false);
+  }
+
+  function error(jqXHR, message) {
+    alert(message);
+  }
 
 };
