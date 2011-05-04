11adc796df982ae70cf24ea88ded83e2a180379e
Bartek Drozdz <bartek@everyday3d.com>
2011-05-04 17:19:09 +0200

    New animations added to the prototype. There in no animation in the dunes section - this is ok, not a bug.

--- deploy/js/video/VideoPlane.js
+++ deploy/js/video/VideoPlane.js
@@ -4,47 +4,62 @@ var VideoPlane = function( shared, layer, conf ) {
 	var config = conf;
 	var hasDistortion = false;
 	var hasKey = false;
-    
-	VideoLoadRegister[ layer.path ] = 1;
+	var isStatic = layer.path.match("png$") || layer.path.match("jpg$");
 	
-    video = document.createElement( 'video' );
-    video.src = layer.path;
-	video.preload = true;
-	video.load();
-    
-	shared.signals.loadItemAdded.dispatch();
+	this.locked = layer.locked;
+	this.path = layer.path;
 	
-	// emit loaded signal either at canplaythrough event
-	// or after 5 seconds
-	// (this is to get around occasional not firing of 
-	//  canplaythrough event :/)
-
-	video.addEventListener( "canplaythrough", function() { 	
-
-		if ( VideoLoadRegister[ layer.path ] == 1 ) {
+	this.removed = false;
+	this.removeAt = layer.removeAt || 1;
+	
+	if(isStatic) {
+		texture = THREE.ImageUtils.loadTexture(layer.path);
+	} else {
+		VideoLoadRegister[ layer.path ] = 1;
 		
-			shared.signals.loadItemCompleted.dispatch();
-			VideoLoadRegister[ layer.path ] = 2;
-
-		}
-
-	}, false );
-	
-	setTimeout( function() { 
+	    video = document.createElement('video');
+	    video.src = layer.path;  
+		video.preload = true;
+		video.load();
 		
-		if( VideoLoadRegister[ layer.path ] == 1 ) {
+		shared.signals.loadItemAdded.dispatch();
+		
+		// emit loaded signal either at canplaythrough event
+		// or after 10 seconds
+		// (this is to get around occasional not firing of 
+		//  canplaythrough event :/)
+		
+		//video.onerror = function(e) {
+		//	console.log(this.path + " error " + e);
+		//}
 
-			shared.signals.loadItemCompleted.dispatch();
-			VideoLoadRegister[ layer.path ] = 2;
-
-		}
-
-	}, 5000 );
+		video.addEventListener( "canplaythrough", function() { 	
 	
-    texture = new THREE.Texture(video);
-    texture.minFilter = THREE.LinearFilter;
-    texture.magFilter = THREE.LinearFilter;
+			if ( VideoLoadRegister[ layer.path ] == 1 ) {
+			
+				shared.signals.loadItemCompleted.dispatch();
+				VideoLoadRegister[ layer.path ] = 2;
+	
+			}
+	
+		}, false );
+		
+		setTimeout( function() { 
+			
+			if( VideoLoadRegister[ layer.path ] == 1 ) {
+	
+				shared.signals.loadItemCompleted.dispatch();
+				VideoLoadRegister[ layer.path ] = 2;
+			}
+	
+		}, 10000 );
+	  
+	    texture = new THREE.Texture(video);
+	    texture.minFilter = THREE.LinearFilter;
+	    texture.magFilter = THREE.LinearFilter;		
+	}
     
+
     switch ( layer.shaderId ) {
 
         case VIDEO_OPAQUE:
@@ -64,6 +79,15 @@ var VideoPlane = function( shared, layer, conf ) {
 
 		case VIDEO_HALFALPHA:
             shader = VideoShaderSource.halfAlpha;
+            break;
+			
+		case VIDEO_SMARTALPHA:
+            shader = VideoShaderSource.smartAlpha;
+            break;
+			
+		case VIDEO_SMARTALPHA_DISTORT:
+            shader = VideoShaderSource.distortSmartalpha;
+			hasDistortion = true;
             break;
 
         case VIDEO_KEYED:
@@ -98,64 +122,58 @@ var VideoPlane = function( shared, layer, conf ) {
         vertexShader: shader.vertexShader,
         fragmentShader: shader.fragmentShader,
 		depthTest: false
-
+		//,blending: THREE.BillboardBlending
     });
 	
-	//
-	if( !layer.width ) layer.width = ( hasDistortion ) ? 1.104 : 1;
-	if( !layer.height ) layer.height = ( hasDistortion ) ? 1.24 : 1;
+	// 
+	if(!layer.width) layer.width = (hasDistortion) ? 1.104 : 1;
+	if(!layer.height) layer.height = (hasDistortion) ? 1.24 : 1;
     
-    if( hasDistortion ) 
-		this.mesh = new THREE.Mesh( config.grid, material );
-	else 
-		this.mesh = new THREE.Mesh( new THREE.Plane( 1,1,1,1 ), material );
-		
+	var plane;
 	
+    if (hasDistortion) {
+		plane = config.grid;
+	} else {
+		plane = new THREE.Plane(1, 1, 40, 1);
+	}
+	
+	if(layer.paralax) {
+		for (var i = 0; i < plane.vertices.length; ++i) {
+			var px = plane.vertices[i].position.x;
+			var sin = Math.sin( (px + 0.5) * Math.PI );
+			plane.vertices[i].position.z = (1-sin) * 500;
+		}
+	}
+		
+	this.mesh = new THREE.Mesh( plane, material );
+
 	this.mesh.scale.x = layer.width;
 	this.mesh.scale.y = layer.height;
     this.mesh.position.z = layer.z;
+	this.mesh.position.y = layer.y || 0;
     this.mesh.scale.x *= Math.abs(layer.z) * config.adj * config.aspect;
     this.mesh.scale.y *= Math.abs(layer.z) * config.adj;
-	
-	
-	if ( false ) { //hasDistortion) {
+	//this.mesh.doubleSided = true;
 
-		wireMaterial = new THREE.MeshShaderMaterial( {
-			uniforms: uniforms,
-			vertexShader: VideoShaderSource.distortWire.vertexShader,
-			fragmentShader: VideoShaderSource.distortWire.fragmentShader,
-			blending: THREE.BillboardBlending,
-			wireframe: true
-		});
+	this.start = function(t) {
+		if(isStatic) return;
 		
-		this.wireMesh = new THREE.Mesh( config.grid, wireMaterial );
-		this.wireMesh.scale.x = layer.width;
-		this.wireMesh.scale.y = layer.height;
-	    this.wireMesh.position.z = layer.z + 0.1;
-	    this.wireMesh.scale.x *= Math.abs(layer.z) * config.adj * config.aspect;
-	    this.wireMesh.scale.y *= Math.abs(layer.z) * config.adj;
-
-	}
-	
-	this.start = function( t ) {
-		
+		console.log(this.path + " readyState: " + video.readyState);
 		video.currentTime = video.duration * t;
 		video.play();
 		
-		interval = setInterval( function() {
-
-	        if ( video.readyState === video.HAVE_ENOUGH_DATA ) {
-
+		texture.needsUpdate = true;
+		
+		interval = setInterval(function(){
+	        if (video.readyState === video.HAVE_ENOUGH_DATA) {
 	            texture.needsUpdate = true;
-
 	        }
-
-	    }, 1000 / 24);
-
+	    }, 1000 / 20);
 	}
 	
 	this.stop = function() {
-		
+	
+		if(isStatic) return;
 		video.pause();
 		clearInterval( interval );
 
@@ -163,7 +181,7 @@ var VideoPlane = function( shared, layer, conf ) {
 	
 	this.updateUniform = function(mouseX, mouseY) {
 
-		if( !hasDistortion ) return;
+		if(!hasDistortion) return;
 
 		material.uniforms['mouseXY'].value.x = -mouseX * config.aspect;
 		material.uniforms['mouseXY'].value.y = -mouseY;
--- deploy/js/video/VideoShader.js
+++ deploy/js/video/VideoShader.js
@@ -58,6 +58,125 @@ var VideoShaderSource = {
 
 	},
 	
+	distortSmartalpha: {
+
+		uniforms: {
+            "aspect": { type: "f", value: 0 },
+			"map": { type: "t", value: 0, texture: null },
+            "mouseXY": { type: "v2", value: new THREE.Vector2() }
+		},
+
+		vertexShader: [
+
+            "uniform vec2 mouseXY;",
+            "uniform vec2 trail1;",
+            "uniform vec2 trail2;",
+            "uniform vec2 trail3;",
+            "uniform float aspect;",
+
+            "varying vec2 vUv;",
+            "varying vec2 vUvPoly;",
+            "varying vec3 pos;",
+            "varying vec3 posPoly;",
+            "varying vec4 viewPos;",
+            "varying vec4 viewPosPoly;",
+            "varying vec2 projPos;",
+            "varying vec2 projPosPoly;",
+            "varying float distance;",
+            "varying float distancePoly;",
+
+
+			"void main() {",
+				"vUv = uv;",
+                "vUvPoly = uv+vec2(normal.x,normal.y);",
+
+                "viewPos = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+                "viewPosPoly = projectionMatrix * modelViewMatrix * vec4( position-vec3(-normal.x,normal.y,0.), 1.0 );",
+
+                "projPos = vec2(aspect,1.)*vec2(viewPos.x/viewPos.z,viewPos.y/viewPos.z);",
+                "projPosPoly = vec2(aspect,1.)*vec2(viewPosPoly.x/viewPosPoly.z,viewPosPoly.y/viewPosPoly.z);",
+
+                "distance = max(0.,1.0-length(projPos-vec2(mouseXY.x, mouseXY.y)));",
+
+                "float distFade = normal.z*0.8+0.8;",
+
+                "distancePoly = max(0.,distFade-length(projPosPoly-vec2(mouseXY.x, mouseXY.y)));",
+
+                "viewPos.xy = viewPos.xy + normalize(projPos-vec2(mouseXY.x, mouseXY.y))*0.6*pow(distance,1.)*(viewPos.z/10.);",
+                "gl_Position = viewPos;",
+
+			"}"
+
+		].join("\n"),
+
+		fragmentShader: [
+            "uniform sampler2D map;",
+
+            "uniform vec2 mouseXY;",
+            "varying vec2 vUv;",
+            "varying vec2 vUvPoly;",
+            "varying vec3 pos;",
+            "varying vec3 posPoly;",
+            "varying vec4 viewPos;",
+            "varying vec4 viewPosPoly;",
+            "varying vec2 projPos;",
+            "varying vec2 projPosPoly;",
+
+            "varying float distance;",
+            "varying float distancePoly;",
+
+			"void main() {",
+				"vec4 c = texture2D( map, vec2( vUv.x * 0.6666, vUv.y ) );",
+				"vec4 a = texture2D( map, vec2( 0.6666 + vUv.x * 0.3333, vUv.y ) );",
+				"c.a = a.r;",
+
+				"vec4 cPoly = texture2D( map, vec2( vUvPoly.x * 0.6666, vUvPoly.y ) );",
+				"vec4 aPoly = texture2D( map, vec2( 0.6666 + vUvPoly.x * 0.3333, vUvPoly.y ) );",
+				"cPoly.a = aPoly.r;",
+				
+				"if ((distancePoly)>0.7) c = cPoly; ",
+				
+                
+                "if (c.a<=0.2) discard;",
+                "else gl_FragColor = c;",
+			"}"
+
+		].join("\n")
+
+	},
+	
+	smartAlpha : {
+
+		uniforms: {
+			"map": { type: "t", value: 0, texture: null },
+		},
+
+		vertexShader: [
+
+			"varying vec2 vUv;",
+
+			"void main() {",
+				"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+				"vUv = uv;",
+			"}"
+
+		].join("\n"),
+
+		fragmentShader: [
+			"uniform sampler2D map;",
+
+			"varying vec2 vUv;",
+
+			"void main() {",
+				"vec4 c = texture2D( map, vec2( vUv.x * 0.6666, vUv.y ) );",
+				"vec4 a = texture2D( map, vec2( 0.6666 + vUv.x * 0.3333, vUv.y ) );",
+				"gl_FragColor = vec4(c.rgb, a.r);",
+			"}"
+
+		].join("\n")
+
+	},
+	
 	keyed: {
 
 		uniforms: {
