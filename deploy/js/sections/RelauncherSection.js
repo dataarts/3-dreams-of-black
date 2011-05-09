6b61fbb371445618f8ceb797051c1149802c98ab
Jono Brandel <jonobrandel@google.com>
2011-05-09 16:42:20 -0700

    added the return event listener on relaunchersection

--- deploy/js/sections/RelauncherSection.js
+++ deploy/js/sections/RelauncherSection.js
@@ -236,8 +236,9 @@ var RelauncherSection = function( shared ) {
 		enter.style.display = "none";
 		domElement.appendChild(enter);
 
-		enter.addEventListener("click", function(e) {
+		enter.addEventListener("click", handleReturn, false);
 
+		function handleReturn() {
 			shared.signals.showexploration.dispatch();
 			shared.signals.startexploration.dispatch( 'dunes' );
 			var divs = container.getElementsByTagName('div');
@@ -245,8 +246,8 @@ var RelauncherSection = function( shared ) {
 				divs[i].style.display = "block";
 			}
 			enter.style.display = "none";
-
-		}, false);
+			shared.signals.keyup.remove( handleReturn )
+		}
 
 		start.addEventListener("click", function(e) {
 
@@ -285,6 +286,7 @@ var RelauncherSection = function( shared ) {
 				divs[i].style.display = "none";
 			}
 			enter.style.display = "block";
+			shared.signals.keyup.add( handleReturn )
 
 		}, false);
 
