d5b23991c30c3f3389c4c268706d5533e956f0e4
Jono Brandel <jonobrandel@google.com>
2011-05-10 22:03:31 -0700

    try cursor, lemme know what you think

--- deploy/js/effects/PointerImageEffect.js
+++ deploy/js/effects/PointerImageEffect.js
@@ -2,33 +2,73 @@ var PointerImageEffect = function ( shared, url ) {
 
 	SequencerItem.call( this );
 
-	var imgElement = document.createElement( 'img' );
-	imgElement.src = url;
+	var pmouseX = 0;
+	var pmouseY = 0;
+	var height = 14;
+
+	var img = document.createElement( 'div' );
+			img.setAttribute("style", "width: 8px; height: 56px;");
+			img.innerHTML = "<img src = '" + url + "' alt = 'mouse' />";
+
+	var imgElement = document.createElement( 'div' );
+	imgElement.setAttribute("style", "width: 8px; height: 14px;");
 	imgElement.style.position = "absolute";
 	imgElement.style.zIndex = 1000;
-	imgElement.style.pointerEvents = "none";
 	imgElement.style.display = "none";
+	imgElement.style.cursor = "none";
+	imgElement.style.overflow = "hidden";
+	imgElement.appendChild(img);
 	
 	document.body.appendChild( imgElement );
 	
 	this.show = function ( progress ) {
 		
-		document.body.style.cursor = 'url("/files/pointer-transparent.png"), auto';
+		// document.body.style.cursor = 'url("/files/pointer-transparent.png"), auto';
+		document.body.style.cursor = "none";
 		imgElement.style.display = "block";
 
 	};
 
+	shared.signals.mousemoved.add( function() {
+
+		var difX = shared.mouse.x - pmouseX;
+		var difY = shared.mouse.y - pmouseY;
+
+		if(difX > 0) {
+
+			if(difY > 0) {
+				height = -28;
+			} else if (difY < 0){
+				height = -14;
+			}
+
+		} else if(difX < 0) {
+
+			if(difY > 0) {
+				height = -42;
+			} else if(difY < 0) {
+				height = 0;
+			}
+
+		}
+
+	});
+
 	this.hide = function () {
 		
 		imgElement.style.display = "none";
-		document.body.style.cursor = "none";
+		document.body.style.cursor = "auto";
 
 	};
 	
 	this.update = function () {
-		
-		imgElement.style.left = ( shared.mouse.x - 40 ) + "px";
-		imgElement.style.top = ( shared.mouse.y - 6 )+ "px";
+
+		img.style.marginTop = height + "px";
+		imgElement.style.left = ( shared.mouse.x - 4 ) + "px";
+		imgElement.style.top = ( shared.mouse.y - 7 )+ "px";
+
+		pmouseX = shared.mouse.x;
+		pmouseY = shared.mouse.y;
 		
 	};
 
