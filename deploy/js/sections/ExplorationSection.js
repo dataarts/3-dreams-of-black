6a7c601501aa20ec40913706888c85b5b17c17e2
Jono Brandel <jonobrandel@google.com>
2011-05-11 18:23:42 -0700

    first commit of explore UI

--- deploy/js/sections/ExplorationSection.js
+++ deploy/js/sections/ExplorationSection.js
@@ -7,15 +7,54 @@ var ExplorationSection = function ( shared ) {
 	var domElement = document.createElement( 'div' );
 	domElement.style.display = 'none';
 
+	var exitButton, resumeButton;
 
-	// init pause meny TODO!
-	
+	var svgCss = ['	#rome-explore-hex-poly-top:hover polygon,',
+		'	#rome-explore-hex-poly-bottom:hover polygon {',
+		'		opacity: 1.0;',
+		'	}',
+		'	#rome-explore-hex-poly-top,',
+		'	#rome-explore-hex-poly-bottom {',
+		'		cursor: pointer;',
+'	}'].join("\n");
+
+	var rule = document.createTextNode(svgCss);
+  var head = document.getElementsByTagName('head')[0];
+  var style = document.createElement('style');
+
+  if (style.stylesheet) {
+
+      style.styleSheet.cssText = rule.nodeValue;
+
+  } else {
+
+      style.appendChild(rule);
+
+  }
+
+  head.appendChild(style);
+
+	var svgHex = [
+		'<svg id="rome-explore-hex" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:a="http://ns.adobe.com/AdobeSVGViewerExtensions/3.0/" x="0px" y="0px" width="368px" height="318px" viewBox="0 0.972 368 318" overflow="visible" enable-background="new 0 0.972 368 318" xml:space="preserve">',
+		'	<polygon opacity="0.5" points="15.294,185.925 92.024,318.833 168.757,185.925 "/>',
+		'	<polygon opacity="0.5" points="200.114,185.925 276.503,318.239 352.894,185.925 "/>',
+		'	<g id="rome-explore-hex-poly-bottom" onClick="ExplorationSection.handleHexClick(0)">',
+		'		<polygon opacity="0.5" points="198.95,185.925 169.921,185.925 93.168,318.869 275.702,318.869 "/>',
+		'		<text x="156" y="300" font-size="14" font-family="Futura-Std-Bold, Futura, sans-serif" fill="#fff">RESUME</text>',
+		'	</g>',
+		'	<polygon opacity="0.5" points="353.868,134.629 199.535,134.629 185.017,159.776 199.536,184.925 353.471,184.925 368.188,159.433 "/>',
+		'	<polygon opacity="0.5" points="14.728,184.925 169.334,184.925 183.854,159.776 169.335,134.629 14.728,134.629 14.728,133.922 0,159.433 14.728,184.944 "/>',
+		'	<polygon opacity="0.5" points="168.758,133.629 91.827,0.377 14.897,133.629 "/>',
+		'	<g id="rome-explore-hex-poly-top" onClick="ExplorationSection.handleHexClick(1)">',
+		'		<polygon opacity="0.5" points="276.098,0 92.773,0 92.825,0.089 169.922,133.629 198.949,133.629 "/>',
+		'		<text x="168" y="28" font-size="14" font-family="Futura-Std-Bold, Futura, sans-serif" fill="#fff">EXIT</text>',
+		'	</g>',
+		'	<polygon opacity="0.5" points="353.291,133.629 276.7,0.972 200.113,133.629 "/>',
+		'	<polygon fill="#F6F4EC" points="119.282,271.692 54.473,159.433 119.282,47.177 248.902,47.177 313.714,159.433 248.902,271.692 "/>',
+		'</svg>'
+	].join('\n');
+
 	var pauseMenu = document.createElement( 'div' );
-	pauseMenu.style.position = "absolute";
-	pauseMenu.style[ "z-index" ] = 100;
-	pauseMenu.innerHTML = "<a href=''>RESUME</a><br/><a href=''>QUIT</a><br/><a href=''>MUTE</a>";
-
-
 
 	// renderer and post effects
 
@@ -79,6 +118,13 @@ var ExplorationSection = function ( shared ) {
 	//--- start explore ---
 
 	function startExplore( worldId ) {
+
+		// UI
+		pauseMenu.style.position = "absolute";
+		pauseMenu.style.zIndex = 1000;
+		pauseMenu.innerHTML = svgHex;
+		pauseMenu.style.display = "none";
+		domElement.appendChild( pauseMenu );
 
 		world   = shared.worlds[ worldId ];
 		portals = world.portals;
@@ -158,26 +204,27 @@ var ExplorationSection = function ( shared ) {
 
 	function stop(e) {
 
-		console.log( "STOP!" );
-
 		if(e.keyCode == 13) {
 
-		 	paused = !paused;
-		
-		 	if( paused ) {
-
-				domElement.appendChild( pauseMenu );
-
-		 	} else {
-		 		
-		 		domElement.removeChild( pauseMenu );
-		 		
-		 	}
+			toggleDisplay();
 		 
 		}
 
 	};
 
+	function toggleDisplay() {
+		paused = !paused;
+	
+	 	if( paused ) {
+
+			pauseMenu.style.display = "block";
+
+	 	} else {
+	 		
+			pauseMenu.style.display = "none";
+	 		
+	 	}
+	}
 
 	//--- update viewport size ---
 
@@ -207,10 +254,21 @@ var ExplorationSection = function ( shared ) {
 
 	this.show = function () {
 
+		if( !shared.hasExplored ) {
+
+			shared.hasExplored = true;
+
+		}
+
 		domElement.style.display = 'block';
 		domElement.appendChild( renderer.domElement );
 		shared.signals.keyup.add( stop );
 
+		pauseMenu.style.display = "none";
+		pauseMenu.style.top = (window.innerHeight / 2.0 - 184) + "px";
+		pauseMenu.style.left = (window.innerWidth / 2.0 - 159) + "px";
+
+		paused = false;
 	};
 
 	this.hide = function () {
@@ -223,12 +281,13 @@ var ExplorationSection = function ( shared ) {
 			environmentSound.pause();
 			
 		}
-
 	};
 
 	this.resize = function ( width, height ) {
 
 		// console.log( "Exploration Resize" );
+		pauseMenu.style.top = (window.innerHeight / 2.0 - 184) + "px";
+		pauseMenu.style.left = (window.innerWidth / 2.0 - 159) + "px";
 
 	};
 
@@ -245,7 +304,6 @@ var ExplorationSection = function ( shared ) {
 		
 		
 		// update world
-
 		if( !paused ) {
 
 			if( world && world.scene ) {
@@ -298,6 +356,34 @@ var ExplorationSection = function ( shared ) {
 
 			}
 
+		}
+
+	};
+
+	ExplorationSection.handleHexClick = function(args) {
+
+		switch(args) {
+			case 0:
+				toggleDisplay();
+				break;
+			case 1:
+				shared.signals.showrelauncher.dispatch();
+				break;
+			case 2:
+				if ( ENV_SOUND_ENABLED ) {
+
+					environmentSound.pause();
+					ENV_SOUND_ENABLED = false;
+
+				} else {
+
+					environmentSound.play();
+					ENV_SOUND_ENABLED = true;
+
+				}
+				break;
+			default:
+				toggleDisplay();
 		}
 
 	};
