32c678cb789ca0f4ffc05cd00f3ae0c02f98047b
Doug Fritz <dougfritz@gmail.com>
2011-05-12 17:10:23 -0700

    history

--- deploy/gallery/js/gallery.js
+++ deploy/gallery/js/gallery.js
@@ -282,7 +282,7 @@ var GALLERY = function(params) {
 
   function populateLightbox(data) {
     activeObject = data;
-    window.location.search = "id="+activeObject.id;
+    history.pushState(null,activeObject.title,window.location.pathname+"?id="+activeImage.id)
     $('#lightbox-title').html(activeObject.title);
     var savedVote = localStorage.getItem(LOCAL_STORAGE_VOTE_PREFIX + activeObject.id);
     if (savedVote == 1) {
