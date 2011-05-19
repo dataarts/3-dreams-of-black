7bf95497df9ebf619a7252fd0108228fc27be719
Mikael Emtinger <mikael.emtinger@gmail.com>
2011-05-19 10:14:32 +0200

    Moved city one tile to the right.

--- deploy/js/worlds/DunesWorld.js
+++ deploy/js/worlds/DunesWorld.js
@@ -108,7 +108,7 @@ var DunesWorld = function ( shared ) {
 
 				tileRow.push( 5 );										// prairie
 
-			} else if( x === 0 && z === 3 ) {
+			} else if( x === 1 && z === 3 ) {
 
 				tileRow.push( 6 );										// city
 
